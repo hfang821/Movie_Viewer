ed0775a5ffc3deb3124c213dcc8300091a864100
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-09-21 00:33:02 -0400

    set up prelinminary client-side files

---
+++ client/src/pages/main.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { getMovies, createMovie, updateMovie, deleteMovie} from '../utils/API';
+
+export default function Main() {
+
+    return (
+        <h1>Hello World!</h1>
+    )
+}
