b5f1350ee47770ea64ebac12da685912679600f5
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-09-22 14:31:18 -0400

    deploy using a different mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/moviedb',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
